925291d0462e390c8e1166a4a699ee2ae605160f
Venz Andrew Dagohoy <venzdagohoy5@gmail.com>
2026-07-03 16:12:59 +0800

    2nd commit

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const btn = document.getElementById("nope-btn");
 const counter = document.getElementById("counter");
 
+const taunts = [
+    "Too slow!",
+    "Missed me!",
+    "Nope!",
+    "Try again!",
+    "Is that all?",
+    "Almost!",
+    "Not even close!",
+    "Catch me!",
+    "Haha!",
+    "Need glasses?"
+];
+
 let count = 0;
-let scale = 1.0;
 
 btn.addEventListener("mouseover", () => {
     counter.innerText = ++count;
@@ -10,15 +22,22 @@ btn.addEventListener("mouseover", () => {
     const x = Math.random() * (window.innerWidth - btn.offsetWidth);
     const y = Math.random() * (window.innerHeight - btn.offsetHeight);
 
+    const randomHue = Math.floor(Math.random() * 360);
+    const randomColor = `hsl(${randomHue}, 100%, 50%)`;
+
+    const randomTaunt = taunts[Math.floor(Math.random() * taunts.length)];
+
     btn.style.left = `${x}px`;
     btn.style.top = `${y}px`;
+    btn.style.backgroundColor = randomColor;
+    btn.innerText = randomTaunt;
 });
 
 btn.addEventListener("click", () => {
     alert("HOW DID YOU DO THAT?! You win!");
 
     counter.innerText = count = 0;
-    scale = 1.0;
 
-    btn.style.cssText = "top: 50%; left: 50%; transform: scale(1);";
+    btn.style.cssText = "top: 50%; left: 50%; transform: scale(1); background-color: red;";
+    btn.innerText = "Click Me!";
 });
